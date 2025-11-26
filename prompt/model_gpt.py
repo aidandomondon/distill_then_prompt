c36d3725209055465a48c1c9fb8eb10ecd5dadee
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2025-11-25 23:29:48 -0500

    Redirect `model` attribute to `self.transformer.model`

--- prompt/model_gpt.py
+++ prompt/model_gpt.py
@@ -166,3 +166,4 @@ class GPTPromptTuningMixin:
 class GPTPromptTuningLM(GPTPromptTuningMixin, GPT2LMHeadModel):
     def __init__(self, config):
         super().__init__(config)
+        self.model = self.transformer.model
