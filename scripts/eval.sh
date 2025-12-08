e61e4114c7b2faa64b85f014cc8affe42d952d0e
aidandomondon <93887720+aidandomondon@users.noreply.github.com>
2025-12-07 23:14:20 -0500

    Fix checkpoint retrieval

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,4 +1,5 @@
 model=MiniLLM/teacher-gpt2-1.5B # Model from which to instantiate tokenizer
+prompt_tuning_dataset=c4 # Dataset used to train prompts
 
 for dataset in wikitext2 ptb c4; do
 
@@ -8,7 +9,7 @@ for opt in adamw; do
 for lr in 0.001; do
 for steps in 300 1000; do
 for soft_token_num in 25; do
-for ckpt in ./generated_prompts/${opt}_lr${lr}_steps${steps}_token${soft_token_num}/${dataset}/best.pth; do
+for ckpt in ./generated_prompts/${opt}_lr${lr}_steps${steps}_token${soft_token_num}/${prompt_tuning_dataset}/best.pth; do
 
 echo "Evaluating ${model_name_or_path} model with ${soft_token_num}-token prompt from ${ckpt} generated with ${steps} steps, ${lr} learning rate, and ${opt} optimizer."
 LOG_FILE_NAME=./logs/evaluation/kd_${opt}_lr${lr}_${dataset}_steps${steps}_token${soft_token_num}_dataset${dataset}.txt
